ac8686c5509542bb24b2c7e4737a064d1c1ec041
francis556 <vikrammehra244@gmail.com>
2022-04-04 22:31:52 +0200

    php and html

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Learning</title>
 
-$greeting = "Ayume Francis";
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center
+        }
+    </style>
+</head>
+<body>
+    <header>
 
-echo "Hello, $greeting";
+        <h1>
+            
+            <?= "Hello, " .  htmlspecialchars($_GET['name']);?>
+        
+        </h1>
 
-
-?>
+    </header>
+    
+    
+</body>
+</html>
