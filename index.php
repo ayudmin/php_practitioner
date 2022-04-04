25adbab6aa2d789f973c87cd429f7f09ddce0166
francis556 <vikrammehra244@gmail.com>
2022-04-04 22:38:02 +0200

    separating php logic from html

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php Learning</title>
+<?php 
 
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center
-        }
-    </style>
-</head>
-<body>
-    <header>
 
-        <h1>
-            
-            <?= "Hello, " .  htmlspecialchars($_GET['name']);?>
-        
-        </h1>
+$greeting = "Hello world";
 
-    </header>
-    
-    
-</body>
-</html>
+require 'index.view.php';
