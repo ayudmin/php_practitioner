5602912d9244fc3878820f85fc0c173499321372
francis556 <vikrammehra244@gmail.com>
2022-04-11 18:17:16 +0200

    intro to PDO

--- functions.php
+++ functions.php
@@ -1,6 +1,32 @@
 <?php 
 
 
+
+function connectToDb()
+{
+    try {
+
+        return  new PDO('mysql:host=localhost;dbname=mytodo', 'root', 'mansnothot');
+    
+    } catch (PDOException $e){
+        
+        die($e->getMessage());
+    }
+
+
+}
+
+
+function fetchAllTasks($pdo)
+{
+    $statement = $pdo->prepare('select * from todos');
+
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+
+}
+
 function dd($data){
 
 
--- index.php
+++ index.php
@@ -1,48 +1,10 @@
 <?php 
 
+require 'Task.php';
+require 'functions.php';
 
+$pdo = connectToDb();
 
-class Task {
-
-    public $description;
-
-    public $completed = false;
-
-    public function __construct($description)
-
-    {
-        $this->description = $description;
-
-    }
-
-
-    public function complete()
-
-    {
-
-        $this->completed = true;
-
-    }
-
-    public function isComplete()
-
-    {
-        return $this->completed;
-    }
-
-}
-
-$tasks = [
-
-    new Task('Go surfing'),
-
-    new Task('Finish my screencast'),
-
-    new Task('Clean my room')
-];
-
-
-$tasks[0]->complete();
-
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
