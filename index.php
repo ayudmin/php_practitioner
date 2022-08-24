9ac5ebf7c00d334532d7bdd352f2f41a37f5d5ad
francis556 <vikrammehra244@gmail.com>
2022-08-24 12:03:33 +0200

    separate php logic from presentation

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Php learning</title>
-    <style type="text/css">
-        
-        header {
+<?php
 
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
 
-    <header>
-        <h1><?= "Hello,  your input is " . htmlspecialchars($_GET['name']); ?></h1>
-    </header>
+$greeting = 'Hello, world';
 
-</body>
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Php learning</title>
+    <style type="text/css">
+        
+        header {
+
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>
+            <?= $greeting; ?>
+        </h1>
+    </header>
+
+</body>
+</html>
