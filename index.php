e163d79ad3649c2a116d65f26cf85ede29e10c2b
francis556 <vikrammehra244@gmail.com>
2022-08-24 09:54:59 +0200

    variables

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-<?php 
+<?php
 
-require 'vendor/autoload.php';
+$name = 'Ayume Francis';
 
-require 'core/bootstrap.php';
-
-
-use App\Core\{Router, Request};
-
-
-
-Router::load('app/routes.php')
-
-    ->direct(Request::uri(), Request::method());
- 
-
+echo "hello, {$name}" ;
